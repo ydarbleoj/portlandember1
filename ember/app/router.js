94bce664af81543c3d67bfb425a1c3eaf73ee627
ydarbleoj <brady.joel@gmail.com>
2014-06-20 10:38:44 -0700

    Initial commit

---
+++ ember/app/router.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+var Router = Ember.Router.extend({
+  location: Portlandember1ENV.locationType
+});
+
+Router.map(function() {
+});
+
+export default Router;
---
+++ ember/tests/helpers/resolver.js
@@ -0,0 +1,9 @@
+import Resolver from 'ember/resolver';
+
+var resolver = Resolver.create();
+
+resolver.namespace = {
+  modulePrefix: 'portlandember1'
+};
+
+export default resolver;
